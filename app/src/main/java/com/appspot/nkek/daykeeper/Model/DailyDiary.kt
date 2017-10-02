ca75263c86f4fa43265177e9833951b6a7c0a8da
eunkyung.namkung <skarnd88@gmail.com>
2017-10-03 06:42:06 +0900

    diary id

--- app/src/main/java/com/appspot/nkek/daykeeper/Model/DailyDiary.kt
+++ app/src/main/java/com/appspot/nkek/daykeeper/Model/DailyDiary.kt
@@ -3,6 +3,6 @@ package com.appspot.nkek.daykeeper.Model
 /**
  * Created by nkek on 2017. 10. 3..
  */
-class DailyDiary constructor(diaryTitle: String, diaryContents: String, diataDate: Long){
-    constructor(diaryTitle: String, diaryContents: String, diataDate: Long, weather: String) : this(diaryTitle, diaryContents, diataDate)
+class DailyDiary constructor(diaryID: Int, diaryTitle: String, diaryContents: String, diataDate: Long){
+    constructor(diaryID: Int,diaryTitle: String, diaryContents: String, diataDate: Long, weather: String) : this(diaryID, diaryTitle, diaryContents, diataDate)
 }
