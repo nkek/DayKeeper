adf846acfd251e29c18ddc7cd1b1509f23974480
eunkyung.namkung <skarnd88@gmail.com>
2017-11-28 22:19:56 +0900

    add pager

--- app/src/main/java/com/appspot/nkek/daykeeper/View/StartInfoActivity.kt
+++ app/src/main/java/com/appspot/nkek/daykeeper/View/StartInfoActivity.kt
@@ -1,9 +1,14 @@
 package com.appspot.nkek.daykeeper.View
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.view.ViewPager
+import android.view.View
 import com.appspot.nkek.daykeeper.R
+import com.appspot.nkek.daykeeper.View.start.StartInfoAdapter
+import kotlinx.android.synthetic.main.activity_startinfo.*
 
 /**
  * Created by nkek on 2017. 11. 5..
@@ -12,8 +17,21 @@ class StartInfoActivity: Activity() {
 
     var pager:ViewPager ?= null
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startinfo)
+
+        introViewPager.adapter = StartInfoAdapter(this, listOf(getDrawable(R.drawable.intro_1),getDrawable(R.drawable.intro_2),getDrawable(R.drawable.intro_3),getDrawable(R.drawable.intro_4)))
+        introViewPagerIndicator.setViewPager(introViewPager)
+        introViewPagerIndicator.setOnPageChangeListener(getPagerSynchronizer(introViewPager))
+    }
+
+    private fun getPagerSynchronizer(vip: ViewPager): ViewPager.SimpleOnPageChangeListener{
+        return object : ViewPager.SimpleOnPageChangeListener(){
+            override fun onPageSelected(position: Int) {
+                vip.setCurrentItem(position, true)
+            }
+        }
     }
 }
--- app/src/main/java/com/appspot/nkek/daykeeper/View/start/StartInfoAdapter.kt
+++ app/src/main/java/com/appspot/nkek/daykeeper/View/start/StartInfoAdapter.kt
@@ -1,30 +1,32 @@
 package com.appspot.nkek.daykeeper.View.start
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.support.v4.view.PagerAdapter
 import android.view.View
 import android.os.Parcelable
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.widget.FrameLayout
 import android.widget.ImageView
 import com.appspot.nkek.daykeeper.R
+import kotlinx.android.synthetic.main.layout_start_info.view.*
 
 /**
  * Created by nkek on 2017. 11. 18..
  */
-class StartInfoAdapter(val context: Context,val list:List<String>): PagerAdapter() {
+open class StartInfoAdapter(val context: Context,val list:List<Drawable>): PagerAdapter() {
 
-    override fun instantiateItem(view: ViewGroup, position: Int): Any {
-        val imageLayout = LayoutInflater.from(context).inflate(R.layout.layout_start_info, view,false)!!
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = LayoutInflater.from(context).inflate(R.layout.layout_start_info, container,false)!!
+        val frame : FrameLayout = view.findViewById(R.id.backgroundLayout) as FrameLayout
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            frame.background = list[position]
+        }
 
-        val imageView = imageLayout!!
-                .findViewById(R.id.image) as ImageView
-
-       // imageView.setImageResource(list.get(position))
-
-        view.addView(imageLayout, 0)
-
-        return imageLayout
+        container.addView(view)
+        return view
     }
 
     override fun isViewFromObject(view: View, Any: Any): Boolean {
@@ -36,8 +38,8 @@ class StartInfoAdapter(val context: Context,val list:List<String>): PagerAdapter
     override fun saveState(): Parcelable? {
         return null
     }
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as View)
+    override fun destroyItem(container: ViewGroup, position: Int, any: Any) {
+        container.removeView( any as View)
     }
 
     override fun getCount(): Int {
